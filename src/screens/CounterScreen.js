cc60ad04f4fb0bc6d1bf6802e02f896bc23d9919
Milan sheladiya <milansheladiya007@gmail.com>
2021-11-02 08:55:17 -0400

    Added Counter Screen

---
+++ src/screens/CounterScreen.js
@@ -0,0 +1,29 @@
+import React,{ useState } from "react";
+import { View,Text, StyleSheet, Button } from "react-native"
+
+const CounterScreen = () => {
+    //let counter = 0;
+    const [counter, setCounter] = useState(0);
+    const [num] = useState(0);
+
+
+    return(
+
+        <View>
+            <Button title="Increase" onPress={() => {
+                setCounter(counter+1);
+                this.setState
+            }}/>
+            <Button title="Decrease" onPress={() => {
+                setCounter(counter-1);
+            }}/>
+            <Text> Current Counter : {counter} </Text>
+        </View>
+
+    );
+
+}
+
+const Style = StyleSheet.create({});
+
+export default CounterScreen;
